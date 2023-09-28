df0ed19c3b9152049a728bbad3682e7e72f0caa4
vivihanehkist <vivihaneh.kist@gmail.com>
2023-09-28 15:26:44 -0300

    commit inicial

--- bin/calculadora_imc.dart
+++ bin/calculadora_imc.dart
@@ -1,5 +1,37 @@
-import 'package:calculadora_imc/calculadora_imc.dart' as calculadora_imc;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${calculadora_imc.calculate()}!');
+void main() {
+  print("Informe o seu peso (em quilogramas): ");
+  var peso = double.parse(stdin.readLineSync() as String);
+
+  print("Informe a sua altura (em metros): ");
+  var altura = double.parse(stdin.readLineSync() as String);
+
+  var imc = calcularIMC(peso, altura);
+  print("Seu IMC é: $imc");
+  interpretarIMC(imc);
+}
+
+double calcularIMC(double peso, double altura) {
+  return peso / (altura * altura);
+}
+
+void interpretarIMC(double imc) {
+  if (imc < 16) {
+    print("Magreza grave");
+  } else if (imc <= 16 && imc <= 17) {
+    print("Magreza moderada");
+  } else if (imc < 18.5) {
+    print("Magreza leve");
+  } else if (imc >= 18.5 && imc < 25) {
+    print("Saudável");
+  } else if (imc >= 25 && imc < 30) {
+    print("Sobrepeso");
+  } else if (imc >= 30 && imc < 35) {
+    print("Obesidade Grau I");
+  } else if (imc >= 35 && imc < 40) {
+    print("Obesidade Grau II (severa)");
+  } else {
+    print("Obesidade Grau III (Mórbida)");
+  }
 }
